9959e254f7cafdedf25e52ff8b5078d07cd6f287
francescomicucci <francesco.micucci@mail.polimi.it>
2024-09-11 17:11:19 +0200

    Changes on circuit_gen.py

--- src/circuit_gen.py
+++ src/circuit_gen.py
@@ -1,16 +1,27 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 import random
 from networkx.drawing.nx_pydot import graphviz_layout
 from qiskit import QuantumCircuit
 
 class RandomCircuit:
-    def __init__(self, num_qubits, num_modules):
-        self.num_qubits       = num_qubits
-        self.num_modules      = num_modules
-        self.dependency_graph = nx.random_tree(self.num_modules)
-        self.dependency_graph = nx.DiGraph([(u,v) for (u,v) in self.dependency_graph.edges() if u<v])
+    def __init__(self, num_modules, module_max_qubits, module_max_gates):
+        self.num_modules       = num_modules
+        self.module_max_gates  = module_max_gates
+        self.module_max_qubits = module_max_qubits
+        self.dependency_graph  = nx.random_tree(self.num_modules)
+        self.dependency_graph  = nx.DiGraph([(u,v) for (u,v) in self.dependency_graph.edges() if u<v])
+        self.modules           = []  
 
+    def gen_random_circuit(self):
+        """Generate a random circuit."""
+        # Generate adjacency matrix from the graph
+        adj_matrix = nx.to_numpy_array(self.dependency_graph, nodelist=sorted(self.dependency_graph.nodes()))
+        
+        # Print the adjacency matrix
+        print(adj_matrix)             
+    
     def draw_dependency_graph(self):
         """Draw the dependency graph of the circuit."""
         # Generate layout for the graph
@@ -32,8 +43,9 @@ class RandomCircuit:
         for _ in range(num_gates):
             gate = random.choice(['h', 'x', 'y', 'z', 'cx'])
             if gate == 'cx':
-                qubits = random.sample(range(num_qubits), 2)
-                module.cx(qubits[0], qubits[1])
+                if num_qubits > 1:
+                    qubits = random.sample(range(num_qubits), 2)
+                    module.cx(qubits[0], qubits[1])
             else:
                 qubit = random.choice(range(num_qubits))
                 getattr(module, gate)(qubit)
