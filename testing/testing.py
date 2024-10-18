0040fa893bd2bf665f9bd561bad9223c395abf2b
francescomicucci <francesco.micucci@mail.polimi.it>
2024-10-18 19:44:02 +0200

    Removed unused files and refactored important ones for better code structure

--- testing/testing.py
+++ testing/testing.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pickle
 
 # Manually specify the path to the 'src' directory
 src_path = os.path.abspath(os.path.join(os.getcwd(), './src'))
@@ -7,22 +8,21 @@ src_path = os.path.abspath(os.path.join(os.getcwd(), './src'))
 # Add the specified path to the system path
 sys.path.append(src_path)
 
-# Third-party libraries
-import networkx as nx
-from qiskit import transpile
-from qiskit_ibm_runtime.fake_provider import FakeGuadalupeV2
-import mapomatic as mm
-
 # Local application/library imports
+import backend_gen as backendGen
 import circuit_gen as circuitGen
-import max_clique as maxClique
 import qubit_mapping as qMap
 
-
-num_modules       = 4
-module_max_qubits = 4
-module_max_gates  = 6
-buffer_distance   = 1
+num_modules        = 4
+module_max_qubits  = 4
+module_max_gates   = 6
+buffer_distance    = 0
+reduced_distance   = 3
+max_allowed_weight = 3
+num_qubits_x       = 5
+num_qubits_y       = 5
+heuristic          = False
+save_backend       = False
 
 # Generate random circuit
 circuit = circuitGen.RandomCircuit(num_modules, module_max_qubits, module_max_gates)
@@ -32,5 +32,17 @@ circuit.gen_random_circuit()
 circuit.draw_dependency_graph()
 
 # Generate the Quantum Circuit Mapping
-q_map = qMap.QubitMapping(circuit, backend=FakeGuadalupeV2(), buffer_distance=buffer_distance)
+q_map = qMap.QubitMapping(
+    circuit, 
+    backend=backendGen.generate_regular_backend(num_qubits_x, num_qubits_y),
+    buffer_distance=buffer_distance, 
+    reduced_distance=reduced_distance, 
+    max_allowed_weight=max_allowed_weight,
+    heuristic=heuristic
+)
 q_map.generate_qubit_mapping()
+
+# Save the backend
+if save_backend:
+    with open('backends/backend.pkl', 'wb') as file:
+        pickle.dump(q_map.backend, file)
