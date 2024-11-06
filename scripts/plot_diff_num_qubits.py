396d784f312865ae599522674f7d640c3aaaac11
Giorgio Miani <giorgio.miani@mail.polimi.it>
2024-11-06 15:16:45 +0100

    add plots for all opt level

--- scripts/plot_diff_num_qubits.py
+++ scripts/plot_diff_num_qubits.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-diff_qubits = {}
-opt_lvl = 3
-for num_modules in range(4,8):
-    path = f'./results/optlvl{opt_lvl}_nm{num_modules}_mmq4_mmg6_rdNone_maw5_nqx100_nqy100_hFalse/total_metrics.csv'
-    df   = pd.read_csv(path)
+import os
 
-    total_qubits = df['total_qubits']
-    qiskit_total_qubits = df['qiskit_total_qubits']
+diff_qubits = {'Num_modules':[], 'Diff_qubits':[], 'Opt_lvl':[]}
+for opt_lvl in range(1,4):
+    for num_modules in range(4,8):
+        path = f'./results/optlvl{opt_lvl}_nm{num_modules}_mmq4_mmg6_rdNone_maw5_nqx100_nqy100_hFalse/total_metrics.csv'
+        if not os.path.exists(path):
+            continue
+        
+        df   = pd.read_csv(path)
 
-    diff = (qiskit_total_qubits - total_qubits)
-    diff_qubits[num_modules] = diff.mean()
+        total_qubits = df['total_qubits']
+        qiskit_total_qubits = df['qiskit_total_qubits']
 
-modules = list(diff_qubits.keys())
-differences = list(diff_qubits.values())
+        diff = (qiskit_total_qubits - total_qubits)
+        diff_qubits['Num_modules'].append(num_modules)
+        diff_qubits['Diff_qubits'].append(diff.mean())
+        diff_qubits['Opt_lvl'].append(opt_lvl)
+
+df = pd.DataFrame(diff_qubits)
+
 
 # Crea il bar plot
+palette = ['lightblue','violet','pink']
 plt.figure(figsize=(10, 6))
-plt.bar(modules, differences, color='skyblue')
+sns.barplot(x='Num_modules', y='Diff_qubits', hue='Opt_lvl', data=df, palette=palette)
 plt.xlabel('Number of Modules in Circuit')
 plt.ylabel('Average Number of Qubits Difference')
 plt.title('Average Number of Qubits Difference: Qiskit vs Proposed Algorithm')
-plt.savefig(f'./plots/diff_qubits_optlvl{opt_lvl}.png')
+plt.savefig(f'./plots/diff_qubits.png')
